b35d720d5bc5692dad31d8c42c58f1114631c5fa
IEVN1003-22002257 <jonymendez40@gmail.com>
2025-09-06 00:14:54 -0600

    Segundo comit 05-09-25

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")
+
+var num1=10;
+let num2=20;
+const num3=30;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+let sum = num1 + num2 + num3;
+console.log("El resultado de la suma es: " + sum)
+
+let matricula:number|string
+matricula= 123456789
+matricula='ASDFG2743'
+console.log(matricula)
