0e38c0d0675d7bc00f1cf96088e541d107750411
IEVN1003-22002257 <jonymendez40@gmail.com>
2025-09-09 18:37:26 -0600

    Desestructuracion de funciones

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import './temasTS/01-tipos-bas.ts'
+import './temasTS/02-obj-interfaz-arre.ts'
+import './temasTS/03-funciones.ts'
+import './temasTS/04-desestructuracion-ob-arre.ts'
+import './temasTS/05-desestructuracion-funciones.ts'
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
